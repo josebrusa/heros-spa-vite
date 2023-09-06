bba7e28f9daddd94a0f511d28dc939b02387d524
josebrusa <josebrusa@outlook.com>
2023-09-06 17:11:39 +0200

    mostrando las cards

---
+++ src/heroes/components/HeroCard.jsx
@@ -0,0 +1,59 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const CharacterByHero = ({ characters, alter_ego }) => {
+    // if (alter_ego === characters) return <></>;
+    // return <p>{characters}</p>;
+
+    return alter_ego === characters ? <></> : <p>{characters}</p>;
+};
+
+export const HeroCard = ({
+    id,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters,
+}) => {
+    const heroImgUrl = `/assets/heros/${id}.jpg`;
+    return (
+        <div className="container" style={{ width: "12rem" }}>
+            <div className="card" style={{ width: "12rem" }}>
+                <img
+                    src={heroImgUrl}
+                    className="card-img-top"
+                    style={{ width: "12rem" }}
+                    alt={superhero}
+                />
+                <div className="card-body" style={{ width: "12rem" }}>
+                    <h2 className="card-title">{superhero}</h2>
+                    <p className="card-text">{alter_ego}</p>
+                    <CharacterByHero
+                        characters={characters}
+                        alter_ego={alter_ego}
+                    />
+                    <p className="card-text">
+                        <small className="text-muted">{first_appearance}</small>
+                    </p>
+                    <p className="card-text">{publisher}</p>
+                    <Link to={`/hero/${id}`}>info...</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+CharacterByHero.propTypes = {
+    alter_ego: PropTypes.string.isRequired,
+    characters: PropTypes.string.isRequired,
+};
+
+HeroCard.propTypes = {
+    id: PropTypes.string.isRequired,
+    superhero: PropTypes.string.isRequired,
+    publisher: PropTypes.string.isRequired,
+    alter_ego: PropTypes.string.isRequired,
+    first_appearance: PropTypes.string.isRequired,
+    characters: PropTypes.string.isRequired,
+};
--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,16 +1,17 @@
 import PropTypes from "prop-types";
 
 import { getHerosByPublisher } from "../helpers";
+import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({ publisher }) => {
     const heroes = getHerosByPublisher(publisher);
 
     return (
-        <ul>
+        <div className="row row-cols-1 row-cols-md-3 g-3">
             {heroes.map((hero) => (
-                <li key={hero.id}>{hero.superhero}</li>
+                <HeroCard key={hero.id} {...hero} />
             ))}
-        </ul>
+        </div>
     );
 };
 
